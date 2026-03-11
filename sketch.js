e55637054822f05ebdbef8600612559693225491
David Vasques <davidevasques12@tecnico.ulisboa.pt>
2026-03-11 23:35:25 +0000

    Change synth note from 'C3' to 'C4'

--- sketch.js
+++ sketch.js
@@ -178,7 +178,7 @@ function mousePressed()
           
           try {
             // Som de ERRO: nota grave, volume no MÁXIMO (1.0), duração maior (0.3s)
-            if (typeof synth !== 'undefined') synth.play('C3', 1.0, 0, 0.3);
+            if (typeof synth !== 'undefined') synth.play('C4', 1.0, 0, 0.3);
           } catch (e) { console.log("Erro no som: ", e); }
         }
         
