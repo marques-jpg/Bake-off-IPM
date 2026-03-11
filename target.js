94d96763eebe7fff47ba39e653fc460c73190d78
Guilherme Marques <gfelicianomarques@gmail.com>
2026-03-11 15:13:33 +0000

    vinculado ao firebase

--- target.js
+++ target.js
@@ -23,43 +23,32 @@ class Target
 
     
     const letterColors = {
-      // Vermelhos e Castanhos (A-D)
-      a: [180, 0, 0],      // Vermelho Puro
-      b: [110, 0, 0],      // Vinho/Bordeaux
-      c: [200, 70, 0],     // Laranja Escuro Brilhante
-      d: [110, 50, 20],    // Castanho Terra
-
-      // Laranjas e Olivas (E-H)
-      e: [160, 100, 0],    // Ouro Escuro
-      f: [130, 130, 0],    // Mostarda / Oliva Claro
-      g: [80, 90, 0],      // Verde Oliva Médio
-      h: [55, 75, 25],     // Verde Tropa Profundo
-
-      // Verdes (I-K)
-      i: [0, 110, 0],      // Verde Floresta
-      j: [0, 70, 40],      // Verde Garrafa
-      k: [30, 100, 80],    // Verde Mar / Turquesa
-
-      // AZUIS: Diferenciação L-O
-      l: [0, 140, 140],    // Ciano Petróleo (Mais esverdeado)
-      m: [20, 80, 200],    // Azul Real (Mais aberto e elétrico)
-      n: [0, 40, 120],     // Azul Marinho Clássico
-      o: [30, 30, 80],     // Azul Noite / Indigo
-
-      // ROXOS E QUENTES: Diferenciação P-U
-      p: [100, 0, 180],    // Roxo Violeta (Frio)
-      q: [150, 0, 150],    // Magenta Puro (Vibrante)
-      r: [160, 0, 80],     // Framboesa / Carmesim (Rosado)
-      s: [130, 40, 60],    // Ameixa / Marsala (Misto/Sóbrio)
-      t: [180, 60, 20],    // Tijolo / Terracota (Laranja-Avermelhado)
-      u: [140, 90, 40],    // Bronze / Ocre Escuro
-
-      // Finalização (V-Z)
-      v: [100, 100, 120],  // Cinza Lavanda
-      w: [70, 70, 70],     // Cinza Médio
-      x: [100, 60, 50],    // Rosa Velho Mate
-      y: [40, 60, 60],     // Ardósia
-      z: [25, 25, 25]      // Quase Preto
+      a: [180, 0, 0],
+      b: [110, 0, 0],
+      c: [200, 70, 0],
+      d: [110, 50, 20],
+      e: [160, 100, 0],
+      f: [130, 130, 0],
+      g: [80, 90, 0],
+      h: [55, 75, 25],
+      i: [0, 110, 0],
+      j: [0, 70, 40],
+      k: [30, 100, 80],
+      l: [0, 140, 140],
+      m: [20, 80, 200],
+      n: [0, 40, 120],
+      o: [30, 30, 80],
+      p: [100, 0, 180],
+      q: [150, 0, 150],
+      r: [160, 0, 80],
+      s: [130, 40, 60],
+      t: [180, 60, 20],
+      u: [140, 90, 40],
+      v: [100, 100, 120],
+      w: [70, 70, 70],
+      x: [100, 60, 50],
+      y: [40, 60, 60],
+      z: [25, 25, 25]
     };
 
 
